adde7381c79e06d4cea3712577fdcd3eafdf11f8
Satlay1512 <115040453+Satlay1512@users.noreply.github.com>
2022-10-16 13:45:50 +0300

    add task006

---
+++ task004/Program.cs
@@ -0,0 +1,9 @@
+﻿// 3. Напишите программу вычисления функции:
+//  x = f(a)
+
+Console.Write("Введите число ");
+string inputstring = Console.ReadLine();
+int value = convert.ToInt32(inputstring);
+int cube = value * value * value; 
+
+System.Console.WriteLine(cobe);
